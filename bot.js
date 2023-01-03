7e700104b23d3146948db6d583626372888e559d
Serhii Prykhozhyi <sprihozhiy@Natalias-MacBook-Air.local>
2023-01-02 21:48:34 -0700

    add express server

---
+++ bot.js
@@ -0,0 +1,33 @@
+const { Telegraf } = require('telegraf');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_API,
+});
+const openai = new OpenAIApi(configuration);
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command('start', (ctx) => {
+    ctx.reply('Hey fella! How can I help you?');
+  });
+  
+bot.on('text', async (ctx) => {
+    // if (ctx.message.text.startsWith('RM bot')) {
+        try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-002',
+            prompt: ctx.message.text,
+            temperature: 0.5,
+            max_tokens: 150
+        });
+        // console.log(response.data);
+        ctx.reply(response.data.choices[0].text);
+        } catch (error) {
+            console.error(error);
+            ctx.reply('Sorry, something went wrong. Please try again later.');
+        }
+    // }
+});
+  
+  bot.launch();
--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-const { Telegraf } = require('telegraf');
-const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config();
+const express = require("express");
+const app = express();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_API,
-});
-const openai = new OpenAIApi(configuration);
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const PORT = process.env.PORT || 4001;
+app.use(express.static("public"));
 
-bot.command('start', (ctx) => {
-    ctx.reply('Hey fella! How can I help you?');
-  });
-  
-bot.on('text', async (ctx) => {
-    // if (ctx.message.text.startsWith('RM bot')) {
-        try {
-        const response = await openai.createCompletion({
-            model: 'text-davinci-002',
-            prompt: ctx.message.text,
-            temperature: 0.5,
-            max_tokens: 150
-        });
-        // console.log(response.data);
-        ctx.reply(response.data.choices[0].text);
-        } catch (error) {
-            console.error(error);
-            ctx.reply('Sorry, something went wrong. Please try again later.');
-        }
-    // }
+app.get("/", (req, res, next) => { 
+    res.status(200).send("RM Fella Bot");
 });
-  
-  bot.launch();
+
+app.listen(PORT, () => {
+    console.log(`Application is running on port ${PORT}`);
+});
